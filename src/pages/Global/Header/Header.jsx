44e467bfbb3a17eb14355725fff95f51a2f27881
risalshahed <risalshahed@gmail.com>
2024-02-02 21:20:22 +0600

    section title component added

--- src/pages/Global/Header/Header.jsx
+++ src/pages/Global/Header/Header.jsx
@@ -3,7 +3,11 @@ import Navbar from './Navbar'
 export default function Header() {
   return (
     <header className='flex justify-between items-center bg-black opacity-80 text-white px-8 py-4 fixed z-10 w-full max-w-screen-xl'>
-      <h2 className='uppercase'>bistro boss<br /><span>restaurant</span></h2>
+      <h2 className='uppercase'>
+        <span className='font-bold text-xl'>bistro boss</span>
+        <br />
+        <span className='tracking-rest'><span className='text-xl'>r</span>estaurant</span>
+      </h2>
       <Navbar />
     </header>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      letterSpacing: {
+        rest: '0.2em'
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
